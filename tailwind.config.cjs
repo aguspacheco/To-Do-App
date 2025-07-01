91f6ca5c0d6a5ff55ca65acd52109bc37728a32c
Agustin <agus_ph_2@outlook.com>
2025-07-01 20:14:57 -0300

    configurando el entorno

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        purple: {
+          600: "#7C3AED",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
